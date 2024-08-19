8e9c50480694391db4579cd47cc66e8dfca7f391
Robin Schneider <robinschneider@t-online.de>
2024-08-19 10:21:05 +0200

    .

--- GaRP/LanguageTool/LanguageTool_Java/src/main/java/com/example/gatewayServer.java
+++ GaRP/LanguageTool/LanguageTool_Java/src/main/java/com/example/gatewayServer.java
@@ -8,7 +8,7 @@ import py4j.GatewayServer;
 
 public class gatewayServer {
     private static final String DICTIONARY_FILE_PATH = "src/Dictionary/CustomDictionaryTest";
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         GatewayServer gatewayServer = new GatewayServer(new DictionaryManager(DICTIONARY_FILE_PATH,new GermanyGerman()));
         gatewayServer.start();
         System.out.println("Gateway Server Started");

0c83218ec667722923a27dc3e9fe7101eda25612
Pfützenbaum <pfuetzenbaum@gmail.com>
2024-09-03 20:18:55 +0200

    Performance Test

--- GaRP/parser/pdfminer_text_extraction.py
+++ GaRP/parser/pdfminer_text_extraction.py
@@ -1,21 +1,22 @@
 # Importieren der notwendigen Bibliotheken
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal, LTChar, LTTextLine, LTAnno
+import re
 
 # Funktion zum Extrahieren von Text aus einer PDF-Datei strukturiert
 def extract_text_from_pdf_structured(pdf_path, starting_page=1, ending_page=100, check_fontname=False, first_lines_to_skip=0, last_lines_to_skip=0):
     """
     Extrahiert Text aus einer PDF-Datei mit Struktur.
 
-    Args:
+    Übergabeparameter:
         pdf_path (str): Pfad zur PDF-Datei
-        starting_page (int, optional): Erste Seite, die extrahiert werden soll. Defaults to 1.
-        ending_page (int, optional): Letzte Seite, die extrahiert werden soll. Defaults to 100.
-        check_fontname (bool, optional): Ob der Fontname überprüft werden soll. Defaults to False.
-        first_lines_to_skip (int, optional): Anzahl der ersten Zeilen, die übersprungen werden sollen. Defaults to 0.
-        last_lines_to_skip (int, optional): Anzahl der letzten Zeilen, die übersprungen werden sollen. Defaults to 0.
+        starting_page (int, optional): Erste Seite, die extrahiert werden soll
+        ending_page (int, optional): Letzte Seite, die extrahiert werden soll
+        check_fontname (bool, optional): Ob der Fontname überprüft werden soll
+        first_lines_to_skip (int, optional): Anzahl der ersten Zeilen, die übersprungen werden sollen
+        last_lines_to_skip (int, optional): Anzahl der letzten Zeilen, die übersprungen werden sollen
 
-    Returns:
+    Rückgabe:
         str: Extrahierter und bereinigter Text
     """
     # Initialisieren der Variablen
@@ -62,7 +63,8 @@ def extract_text_from_pdf_structured(pdf_path, starting_page=1, ending_page=100,
                             # Überprüfen, ob das Zeichen ein LTChar-Objekt ist
                             if isinstance(character, LTChar):
 
-                                # Ersetzen von Ligaturen, da es sonst zu Problemen bei der nachträglichen Verarbeitung der Schriftart kommen kann
+                                # Ersetzen von Ligaturen bereits zu diesem Zeitpunkt
+                                # Ansonsten kann es zu Problemen bei der nachträglichen Verarbeitung der Schriftart kommen kann
                                 match character.get_text():
                                     case "ﬀ":
                                         extracted_text += "ff"
@@ -85,9 +87,16 @@ def extract_text_from_pdf_structured(pdf_path, starting_page=1, ending_page=100,
                                 # Hinzufügen des aktuellen Zeichens zum extrahierten Text
                                 extracted_text += current_char
 
-                            # Überprüfen, ob das Zeichen ein LTAnno-Objekt ist (z.B. ein Leerzeichen oder ein Absatzzeichen)
-                            elif isinstance(character, LTAnno):
-                                # Hinzufügen eines Leerzeichens zum extrahierten Text
+                            # Überprüfen, ob das Zeichen ein LTAnno-Objekt ist (z.B. ein Leerzeichen oder ein Zeilenumbruch)
+                            # elif isinstance(character, LTAnno):
+
+                                # Hinzufügen eines Leerzeichens zum extrahierten Text)
+                                #extracted_text += " "
+                            elif character.get_text() == " ":
+                                extracted_text += " "
+                            elif character.get_text() == "\n":
+                                if extracted_text.endswith("-"):
+                                    extracted_text = extracted_text[:-1]
                                 extracted_text += " "
 
     # Entfernen der ersten beiden Zeichen des extrahierten Textes, da diese gesetzt werden aufgrund der Änderung der Schriftgröße
@@ -106,10 +115,10 @@ def extract_text_from_pdf_pagewise(pdf_path, starting_page=1, ending_page=100, f
 
     Übergabeparameter:
         pdf_path (str): Pfad zur PDF-Datei
-        starting_page (int): Erste Seite, die extrahiert werden soll. Defaults to 1.
-        ending_page (int): Letzte Seite, die extrahiert werden soll. Defaults to 100.
-        first_lines_to_skip (int): Anzahl der ersten Zeilen, die übersprungen werden sollen. Defaults to 0.
-        last_lines_to_skip (int): Anzahl der letzten Zeilen, die übersprungen werden sollen. Defaults to 0.
+        starting_page (int): Erste Seite, die extrahiert werden soll
+        ending_page (int): Letzte Seite, die extrahiert werden soll
+        first_lines_to_skip (int): Anzahl der ersten Zeilen, die übersprungen werden sollen
+        last_lines_to_skip (int): Anzahl der letzten Zeilen, die übersprungen werden sollen
 
     Rückgabe:
         str: Extrahierter und bereinigter Text
@@ -227,47 +236,33 @@ def clean_text(cleaned_text):
     Rückgabe:
         str: Bereinigter Text
     """
-    # Behandlung von doppelten Leerzeichen
-    cleaned_text = cleaned_text.replace("  ", " ")
-    cleaned_text = cleaned_text.replace("- ", "")
+    # Behandlung von doppelten Leerzeichen, Umlauten und Ligaturen
+    # Ersetzen von Mehrfachleerzeichen durch ein einzelnes Leerzeichen, aber keine Zeilenumbrüche
+    cleaned_text = re.sub(r'[^\S\n]+', ' ', cleaned_text)
+
+    # Entfernen von Bindestrichen gefolgt von einem Leerzeichen
+    # cleaned_text = re.sub(r'-\s*', '', cleaned_text)
 
     # Behandlung von Fehlern bei Umlauten
-    cleaned_text = cleaned_text.replace("¨u", "ü")
-    cleaned_text = cleaned_text.replace("¨a", "ä")
-    cleaned_text = cleaned_text.replace("¨o", "ö")
-    cleaned_text = cleaned_text.replace("¨U", "Ü")
-    cleaned_text = cleaned_text.replace("¨A", "Ä")
-    cleaned_text = cleaned_text.replace("¨O", "Ö")
+    cleaned_text = re.sub(r'¨([uaoUOA])', lambda m: {'u': 'ü', 'a': 'ä', 'o': 'ö', 'U': 'Ü', 'A': 'Ä', 'O': 'Ö'}[m.group(1)], cleaned_text)
 
     # Behandlung von Ligaturen
-    cleaned_text = cleaned_text.replace("ﬃ", "fi")
-    cleaned_text = cleaned_text.replace("ﬁ", "fi")
-    cleaned_text = cleaned_text.replace("ﬀ", "ff")
+    cleaned_text = re.sub(r'[ﬃﬁ]', 'fi', cleaned_text)
+    cleaned_text = re.sub(r'ﬀ', 'ff', cleaned_text)
 
     return cleaned_text
 
 # Funktion zum Speichern des Textes in eine Datei
 def save_text_to_file(text, output_file):
-    """
-    Speichert den Text in eine Datei.
-
-    Übergabeparameter:
-        text (str): Text, der gespeichert werden soll
-        output_file (str): Pfad zur Ausgabedatei
-    """
     with open(output_file, "w", encoding="utf-8") as file:
         file.write(text)
 
-# Hauptfunktion
 def main():
-    """
-    Hauptfunktion des Programms.
-    """
     output_file = "C:\\Visual Studio Code\\GaRP\\GaRP-Projekt\\GaRP\\parser\\output_test.txt"  
 
-    filename = "C:\\Visual Studio Code\\GaRP\\GaRP-Projekt\\GaRP\\parser\\test_files\\sample04.pdf"
-    starting_page = 1
-    ending_page = 1
+    filename = "C:\\Visual Studio Code\\GaRP\\GaRP-Projekt\\GaRP\\parser\\test_files\\PA2_Version_7_0.pdf"
+    starting_page = 6
+    ending_page = 10
     
     first_lines_to_skip = 0
 

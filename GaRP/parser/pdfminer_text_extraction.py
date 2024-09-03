cd52585d60f4eb1dbcb75b84b092661ec8bdf58d
Pfützenbaum <pfuetzenbaum@gmail.com>
2024-09-03 17:42:36 +0200

    Adding Comments, minor changes and improvements to pagewise parsing method

--- GaRP/parser/pdfminer_text_extraction.py
+++ GaRP/parser/pdfminer_text_extraction.py
@@ -1,49 +1,68 @@
+# Importieren der notwendigen Bibliotheken
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal, LTChar, LTTextLine, LTAnno
 
+# Funktion zum Extrahieren von Text aus einer PDF-Datei strukturiert
 def extract_text_from_pdf_structured(pdf_path, starting_page=1, ending_page=100, check_fontname=False, first_lines_to_skip=0, last_lines_to_skip=0):
+    """
+    Extrahiert Text aus einer PDF-Datei mit Struktur.
+
+    Args:
+        pdf_path (str): Pfad zur PDF-Datei
+        starting_page (int, optional): Erste Seite, die extrahiert werden soll. Defaults to 1.
+        ending_page (int, optional): Letzte Seite, die extrahiert werden soll. Defaults to 100.
+        check_fontname (bool, optional): Ob der Fontname überprüft werden soll. Defaults to False.
+        first_lines_to_skip (int, optional): Anzahl der ersten Zeilen, die übersprungen werden sollen. Defaults to 0.
+        last_lines_to_skip (int, optional): Anzahl der letzten Zeilen, die übersprungen werden sollen. Defaults to 0.
+
+    Returns:
+        str: Extrahierter und bereinigter Text
+    """
+    # Initialisieren der Variablen
     current_fontname = None
     current_size = 1.0
     extracted_text = ""
 
-    # Initialisierung des Seitenzählers
     current_page = 0
 
+    # Durchlaufen aller Seiten in der PDF-Datei
     for page in extract_pages(pdf_path):
-        
-        # Erhöhe den Seitenzähler
         current_page += 1  
+        # Überspringen von Seiten, die nicht extrahiert werden sollen
         if current_page < starting_page:
-            # Springe zur nächsten Seite, wenn starting_page noch nicht erreicht ist
             continue  
         elif current_page > ending_page:
-            # Beende Schleife, wenn ending_page überschritten wird
             break
         
         current_line = 0
+        # Ermitteln der Gesamtzahl der Zeilen auf der aktuellen Seite
         total_lines = len([elem for elem in page if isinstance(elem, LTTextBoxHorizontal)])
 
+        # Durchlaufen aller Elemente auf der Seite
         for page_element in page:
+            # Überprüfen, ob das Element ein Textfeld ist
             if isinstance(page_element, LTTextBoxHorizontal):
-                # Überspringe die ersten Zeilen der Seite
+                # Überspringen von Zeilen am Seitenanfang
                 if current_line < first_lines_to_skip:
                     current_line += 1
                     continue 
 
-                # Für Footer Entfernung: 
-                # get length of page_element in page
-                # iterate x times less then wanted with a if statement and if x is reached break
+                # Überspringen von Zeilen am Seitenende
                 if current_line >= total_lines - last_lines_to_skip:
                     break
 
                 current_line += 1
 
+                # Durchlaufen aller Textzeilen im Textfeld
                 for text_line in page_element:
+                    # Überprüfen, ob die Textzeile eine LTTextLine-Instanz ist
                     if isinstance(text_line, LTTextLine):
+                        # Durchlaufen aller Zeichen in der Textzeile
                         for character in text_line:
+                            # Überprüfen, ob das Zeichen ein LTChar-Objekt ist
                             if isinstance(character, LTChar):
 
-                                # Problembehandlung von Ligaturen und Aufzählungszeichen
+                                # Ersetzen von Ligaturen, da es sonst zu Problemen bei der nachträglichen Verarbeitung der Schriftart kommen kann
                                 match character.get_text():
                                     case "ﬀ":
                                         extracted_text += "ff"
@@ -55,91 +74,165 @@ def extract_text_from_pdf_structured(pdf_path, starting_page=1, ending_page=100,
                                         extracted_text += "\n•"
                                         continue
                                 
+                                # Überprüfen, ob der Fontname berücksichtigt werden soll
                                 if check_fontname:
+                                    # Aufrufen der Funktion zum Überprüfen des Fontnamens und der Schriftgröße
                                     current_char, current_fontname, current_size = handle_font_change(character, current_fontname, current_size)
                                 else:
-                                    current_char, current_fontname, current_size = handle_size_change(character, current_fontname, current_size)
+                                    # Aufrufen der Funktion zum Überprüfen der Schriftgröße
+                                    current_char, current_fontname, current_size = handle_size_change(character, current_size)
 
+                                # Hinzufügen des aktuellen Zeichens zum extrahierten Text
                                 extracted_text += current_char
 
-                            #LTAnno entspricht Leerzeichen und Seitenumbrüchen (\n)
+                            # Überprüfen, ob das Zeichen ein LTAnno-Objekt ist (z.B. ein Leerzeichen oder ein Absatzzeichen)
                             elif isinstance(character, LTAnno):
+                                # Hinzufügen eines Leerzeichens zum extrahierten Text
                                 extracted_text += " "
 
-    # Entferne die ersten beiden Zeichen des Textes, da sie leer sind (\n\n)
+    # Entfernen der ersten beiden Zeichen des extrahierten Textes, da diese gesetzt werden aufgrund der Änderung der Schriftgröße
     extracted_text = extracted_text[2:]
 
+    # Aufrufen der Funktion zum Bereinigen des Textes
     cleaned_text = clean_text(extracted_text)
 
+    # Rückgabe des bereinigten Textes
     return cleaned_text
 
+# Funktion zum Extrahieren von Text aus einer PDF-Datei seitenweise
 def extract_text_from_pdf_pagewise(pdf_path, starting_page=1, ending_page=100, first_lines_to_skip=0, last_lines_to_skip=0):
+    """
+    Extrahiert Text aus einer PDF-Datei seitenweise.
+
+    Übergabeparameter:
+        pdf_path (str): Pfad zur PDF-Datei
+        starting_page (int): Erste Seite, die extrahiert werden soll. Defaults to 1.
+        ending_page (int): Letzte Seite, die extrahiert werden soll. Defaults to 100.
+        first_lines_to_skip (int): Anzahl der ersten Zeilen, die übersprungen werden sollen. Defaults to 0.
+        last_lines_to_skip (int): Anzahl der letzten Zeilen, die übersprungen werden sollen. Defaults to 0.
+
+    Rückgabe:
+        str: Extrahierter und bereinigter Text
+    """
+    # Initialisieren der Variablen
     extracted_text = ""
     current_page = 0
 
+    # Durchlaufen aller Seiten in der PDF-Datei
     for single_page in extract_pages(pdf_path):
         current_page += 1
+        # Überspringen von Seiten, die nicht extrahiert werden sollen
         if current_page < starting_page:
             continue
         elif current_page > ending_page:
             break
 
         current_line = 0
+        # Ermitteln der Gesamtzahl der Zeilen auf der aktuellen Seite
         total_lines = len([elem for elem in single_page if isinstance(elem, LTTextBoxHorizontal)])
         for element in single_page:
+            # Überprüfen, ob das Element ein Textfeld ist
             if isinstance(element, LTTextBoxHorizontal):
 
-                # Überspringe die ersten Zeilen der Seite
+                # Überspringen von Zeilen am Seitenanfang
                 if current_line < first_lines_to_skip:
                     current_line += 1
                     continue 
 
-                # Für Footer Entfernung: 
-                # get length of page_element in page
-                # iterate x times less then wanted with a if statement and if x is reached break
+                # Überspringen von Zeilen am Seitenanfang
                 if current_line >= total_lines - last_lines_to_skip:
                     break
 
                 current_line += 1
 
+                # Durchlaufen aller Textzeilen im Textfeld
                 for text_line in element:
+                    # Überprüfen, ob die Textzeile eine LTTextLine-Instanz ist
                     if isinstance(text_line, LTTextLine):
+                        # Hinzufügen des Textes der Textzeile zum extrahierten Text
                         extracted_text += text_line.get_text()
+
+                        # Entfernen von Trennstrichen, doppelten Leerzeichen oder Zeilenumbrüchen am Zeilenende
                         if extracted_text.endswith("- "):
                             extracted_text = extracted_text[:-2]
-                        extracted_text = extracted_text.replace("  ", " ")
-                        extracted_text = extracted_text.replace("\n", " ")
+                        elif extracted_text.endswith("\n"):
+                            extracted_text = extracted_text[:-1]
+                            extracted_text += " "
                         
-        # Füge einen Seitenumbruch ein, dass jede Seite in einer neuen Zeile beginnt
-        extracted_text += "Seitenumbruch einfügen"
-    extracted_text = extracted_text.replace("Seitenumbruch einfügen", "\n")
+        # Hinzufügen eines Zeilenumbruchs zum extrahierten Text am Ende jeder Seite
+        extracted_text = extracted_text + "\n"
 
+    # Aufrufen der Funktion zum Bereinigen des Textes
     cleaned_text = clean_text(extracted_text)
 
+    # Rückgabe des bereinigten Textes
     return cleaned_text
 
+# Funktion zum Überprüfen des Fontnamens
 def handle_font_change(character, current_fontname, current_size):
+    """
+    Überprüft den Fontnamen und die Schriftgröße und aktualisiert ihn wenn nötig.
+    Bei Änderungen von mehr als 10% der Schriftgröße oder der Schriftart, wird ein Zeilenumbruch vor dem Zeichen hinzugefügt.
+
+    Übergabeparameter:
+        character (LTChar): Zeichen, das überprüft werden soll
+        current_fontname (str): Aktueller Fontname
+        current_size (float): Aktuelle Schriftgröße
+
+    Rückgabe:
+        tuple: Aktualisiertes Zeichen, Fontname und Schriftgröße
+    """
+    # Überprüfen, ob der Fontname sich geändert hat oder die Schriftgröße um mehr als 10% abweicht
     if character.fontname != current_fontname or abs(character.size - current_size) / current_size >= 0.1:
+        # Aktualisieren des aktuellen Fontnamens und der Schriftgröße
         current_fontname = character.fontname
         current_size = character.size
+        # Rückgabe des aktualisierten Zeichens, Fontnamens und Schriftgröße mit zwei Zeilenumbrüchen vor dem Zeichen
         return "\n\n" + character.get_text(), current_fontname, current_size
     else:
+        # Rückgabe des Zeichens, Fontnamens und Schriftgröße ohne Änderungen
         return character.get_text(), current_fontname, current_size 
 
-def handle_size_change(character, current_fontname, current_size):
+# Funktion zum Überprüfen der Schriftgröße
+def handle_size_change(character, current_size):
+    """
+    Überprüft die Schriftgröße und aktualisiert sie wenn nötig.
+    Bei Änderungen von mehr als 10% der Schriftgröße, wird ein Zeilenumbruch vor dem Zeichen hinzugefügt.
+
+    Übergabeparameter:
+        character (LTChar): Zeichen, das überprüft werden soll
+        current_fontname (str): Aktueller Fontname
+        current_size (float): Aktuelle Schriftgröße
+
+    Rückgabe:
+        tuple: Aktualisiertes Zeichen, Fontname und Schriftgröße
+    """
+
+    # Überprüfen, ob die Schriftgröße sich um mehr als 10% von der aktuellen Schriftgröße unterscheidet
     if abs(character.size - current_size) / current_size >= 0.1:
-        current_fontname = character.fontname
+        # Aktualisieren des aktuellen Fontnamens und der Schriftgröße
         current_size = character.size
-        return "\n\n" + character.get_text(), current_fontname, current_size
+        # Rückgabe des aktualisierten Zeichens, Fontnamens und Schriftgröße mit zwei Zeilenumbrüchen vor dem Zeichen
+        return "\n\n" + character.get_text(), current_size
     else:
-        return character.get_text(), current_fontname, current_size
+        # Rückgabe des Zeichens, Fontnamens und Schriftgröße ohne Änderungen
+        return character.get_text(), current_size 
 
+# Funktion zum Bereinigen des Textes
 def clean_text(cleaned_text):
-    # Gedacht für Silbentrennung am Zeilenende
-    # Problematisch, da es auch regulär in Texten vorkommen kann bsp.: Vor- und Nachteile
-    cleaned_text = cleaned_text.replace("- ", "")
+    """
+    Bereinigt den Text von unnötigen Zeichen.
 
-    # Behandlung von Umlauten aus Latex
+    Übergabeparameter:
+        cleaned_text (str): Text, der bereinigt werden soll
+
+    Rückgabe:
+        str: Bereinigter Text
+    """
+    # Behandlung von doppelten Leerzeichen
+    cleaned_text = cleaned_text.replace("  ", " ")
+
+    # Behandlung von Fehlern bei Umlauten
     cleaned_text = cleaned_text.replace("¨u", "ü")
     cleaned_text = cleaned_text.replace("¨a", "ä")
     cleaned_text = cleaned_text.replace("¨o", "ö")
@@ -154,39 +247,35 @@ def clean_text(cleaned_text):
 
     return cleaned_text
 
-# Zugriff direkt über GUI, keine Einbindung in andere Funktionen
-def except_words(text, words_to_except):
-    for word in words_to_except:
-        text = text.replace(word, "")
-    return text
-
+# Funktion zum Speichern des Textes in eine Datei
 def save_text_to_file(text, output_file):
+    """
+    Speichert den Text in eine Datei.
+
+    Übergabeparameter:
+        text (str): Text, der gespeichert werden soll
+        output_file (str): Pfad zur Ausgabedatei
+    """
     with open(output_file, "w", encoding="utf-8") as file:
         file.write(text)
 
+# Hauptfunktion
 def main():
-    # Standardwerte für die Textextraktion
-    output_file = "GaRP\\parser\\output_test.txt"  
-    plain_text = ""
+    """
+    Hauptfunktion des Programms.
+    """
+    output_file = "C:\\Visual Studio Code\\GaRP\\GaRP-Projekt\\GaRP\\parser\\output_test.txt"  
 
-    # Einstellungen für die Textextraktion, abhängig von der PDF-Datei
-    filename = "GaRP\\parser\\test_files\\PA2_Version_7_0.pdf"
+    filename = "C:\\Visual Studio Code\\GaRP\\GaRP-Projekt\\GaRP\\parser\\test_files\\PA2_Version_7_0.pdf"
     starting_page = 6
     ending_page = 10
     
-    # Anzahl der Zeilen, die zu Beginn jeder Seite übersprungen werden sollen
     first_lines_to_skip = 0
 
-    # Anzahl der Zeilen, die am Ende jeder Seite übersprungen werden sollen
     last_lines_to_skip = 0
 
-    # True: Strukturiert nach Schriftart und Schriftgröße
-    # False: Unstrukturiert, nur Seitenweise Extraktion
     extract_structured = True
 
-    # Notwendig, wenn extract_structured = True
-    # True: Gruppierung nach Schriftart und Schriftgröße
-    # False: Gruppierung nur nach Schriftgröße
     check_fontname = True
 
     if extract_structured:

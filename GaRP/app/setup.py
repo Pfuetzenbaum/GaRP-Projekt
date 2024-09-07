10acb4696685dbd402de292ee3d9ec5da06d94c3
Robin Schneider <robinschneider@t-online.de>
2024-09-07 21:06:23 +0200

    License GPL information added

--- GaRP/app/setup.py
+++ GaRP/app/setup.py
@@ -1,3 +1,18 @@
+"""Copyright (C) 2024 Gantert, Schneider, Sewald
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/> """
+
 import sys
 from cx_Freeze import setup, Executable
 
@@ -9,7 +24,7 @@ build_exe_options = {
 }
 
 build_options = {
-    'build_exe': 'C:\\dev\\GaRP\\GaRP Anwendung'  # Doppelte Backslashes verwenden
+    'build_exe': 'C:\\dev\\GaRP\\GaRP Anwendung' 
 }
 
 

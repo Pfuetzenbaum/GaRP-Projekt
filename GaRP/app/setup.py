c63bf8c6cea759e5f7147285bb586969c705272e
Robin Schneider <robinschneider@t-online.de>
2024-09-08 11:51:27 +0200

    Build new Executable

--- GaRP/app/setup.py
+++ GaRP/app/setup.py
@@ -19,14 +19,10 @@ from cx_Freeze import setup, Executable
 # Build options für cx_Freeze
 build_exe_options = {
     "packages": ["os", "py4j", "customtkinter", "tkinter", "pdfminer"],
-    "include_files": ["integrations/lib/demo-1.0.jar", "integrations/lib/Dictionary/CustomDictionaryGerman"],  # JAR-Datei inkludieren
-    "include_msvcr": True  # Um Microsoft Visual C++ Redistributables zu includieren, falls nötig
+    "include_files": ["integrations/lib/demo-1.0.jar", "integrations/lib/Dictionary/CustomDictionaryGerman", "integrations/lib/Dictionary/CustomDictionaryEnglish"],  # JAR-Datei inkludieren
+    "optimize": 2,
+    "include_msvcr": False  # Um Microsoft Visual C++ Redistributables zu includieren, falls nötig
 }
-
-build_options = {
-    'build_exe': 'C:\\dev\\GaRP\\GaRP Anwendung' 
-}
-
 
 # Festlegen der Basis für die ausführbare Datei
 base = None
@@ -37,7 +33,7 @@ if sys.platform == "win32":
 setup(
     name="GaRP",
     version="1.0",
-    description="GaRP Anwendung",
+    description="GaRP - das Tool zur Grammatik- und Rechtschreibprüfung von PDF-Dateien",
     options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base=base)]  # "app/main.py" als Einstiegspunkt
+    executables=[Executable("main.py", base=base, icon="icon.ico")]  # "app/main.py" als Einstiegspunkt
 )
